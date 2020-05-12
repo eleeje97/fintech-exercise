42662bf2c1a1914d8ae3ca88a3d44d0a69452a50
daeunLee <eleeje97@gmail.com>
2020-05-12 16:30:36 +0900

    ejs example

--- expressServer.js
+++ expressServer.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // ejs file location
+app.set('view engine', 'ejs'); //select view template engine
 
 // root 라우터
 app.get('/', function (req, res) {
-    res.send('<html><h1>title</h1><h2>contents</h2></html>')
+    var title = "javascript"
+    res.send('<html><h1>'+title+'</h1><h2>contents</h2></html>')
+})
+
+// ejs --> view와 logic을 분리
+app.get('/ejs', function (req, res) {
+    res.render('test')
 })
 
 // test 라우터
