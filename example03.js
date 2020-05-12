d0f6e78e3ec1a79151218e852fb7d90c50761186
daeunLee <eleeje97@gmail.com>
2020-05-12 10:52:14 +0900

    example03 added

---
+++ example03.js
@@ -0,0 +1,21 @@
+function multi(p1, p2) {
+    return p1 * p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+function div(p1, p2) {
+    return p1 / p2; // p1, p2 나눗셈연산의 결과를 반환한다.
+}
+
+function plus(p1, p2) {
+    return p1 + p2; // p1, p2 덧셈연산의 결과를 반환한다.
+}
+
+function minus(p1, p2) {
+    return p1 - p2; // p1, p2 뺄셈연산의 결과를 반환한다.
+}
+
+// 결과 출력
+console.log(multi(2,3))
+console.log(div(2,3))
+console.log(plus(2,3))
+console.log(minus(2,3))
